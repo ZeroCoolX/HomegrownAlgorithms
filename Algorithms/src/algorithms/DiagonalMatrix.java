c60d3e4473acbe2b7c499a748099be6dc1068b78
PattonCooper <TheHerbaliser@192.168.1.29>
2015-10-16 22:44:35 -0500

    Added a fun little diagonal matrix calculator which calculates all values upwards from a given split matrix (diagonally) where this diagonal line are the only non zero values

--- Algorithms/src/algorithms/DiagonalMatrix.java
+++ Algorithms/src/algorithms/DiagonalMatrix.java
@@ -11,4 +11,49 @@ package algorithms;
  */
 public class DiagonalMatrix {
     
+    public static void main(String[] args) throws java.lang.Exception {
+        int[][] A = new int[4][4];
+
+        /*fill starting diagnol from (i,j)->(i+1, j+1)->(i+2,j+2)->...->(i+n,j+n)
+         literally just give it any values. everthing higher than this diagonal will be calculated*/
+        A[0][0] = 3;
+        A[1][1] = 2;
+        A[2][2] = 4;
+        A[3][3] = 3;
+        prettyPrint(calculateDiagonals(A));
+    }
+
+    private static int[][] calculateDiagonals(int[][] arr) {
+        int count = 0;
+        while (count < arr.length - 1) {
+            int i = 0;
+            int j = count + 1;
+            while (j <= arr.length - 1) {
+                int i2 = i;
+                int j2 = j;
+                int temp = 0;
+                while (i2 != j && j2 != i) {
+                    //	System.out.println("Multiplying: A[i][i2] * A[j2][j2] = " + A[i][i2] + " * " +A[j2][j]);
+                    temp += (arr[i][i2] * arr[j2][j]);
+                    ++i2;
+                    --j2;
+                }
+                arr[i][j] = temp;
+                ++i;
+                ++j;
+            }
+            ++count;
+        }
+        return arr;
+    }
+    
+    private static void prettyPrint(int[][] arr) {
+        for (int i = 0; i < arr.length; ++i) {
+            for (int j = 0; j < arr.length; ++j) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
 }
